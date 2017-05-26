34d7af189faf03aaa70641fac529b1b3a9eac250
万少 <344277934@qq.com>
2017-05-26 11:16:45 +0800

    change suffix to .md

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,3 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -7,15 +10,13 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.timeout.IdleStateHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Created by wanshao on 2017/5/25.
  */
 public class Client {
 
-    //idle时间
+    // idle时间
     private final static int readerIdleTimeSeconds = 40;
     private final static int writerIdleTimeSeconds = 50;
     private final static int allIdleTimeSeconds = 100;
@@ -25,21 +26,21 @@ public class Client {
         Logger logger = LoggerFactory.getLogger(Client.class);
         logger.info("Welcome");
         Client client = new Client();
-        client.connect("127.0.0.1",5527);
+        client.connect("127.0.0.1", 5527);
     }
-
 
     /**
      * 初始化系统属性
      */
-    private static void initProperties(){
-        System.setProperty("middleware.test.home",Constants.TESTER_HOME);
+    private static void initProperties() {
+        System.setProperty("middleware.test.home", Constants.TESTER_HOME);
         System.setProperty("middleware.teamcode", Constants.TEAMCODE);
         System.setProperty("app.logging.level", Constants.LOG_LEVEL);
     }
 
     /**
      * 连接服务端
+     * 
      * @param host
      * @param port
      * @throws Exception
@@ -53,6 +54,7 @@ public class Client {
             b.channel(NioSocketChannel.class);
             b.option(ChannelOption.SO_KEEPALIVE, true);
             b.handler(new ChannelInitializer<SocketChannel>() {
+
                 @Override
                 public void initChannel(SocketChannel ch) throws Exception {
                     ch.pipeline().addLast(new IdleStateHandler(10, 0, 0));
--- src/main/java/ServerDemoInHandler.java
+++ src/main/java/ServerDemoInHandler.java
@@ -1,5 +1,5 @@
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -8,27 +8,32 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.handler.timeout.IdleState;
-import io.netty.handler.timeout.IdleStateEvent;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * 处理client端的请求
- * Created by wanshao on 2017/5/25.
+ * 处理client端的请求 Created by wanshao on 2017/5/25.
  */
 public class ServerDemoInHandler extends ChannelInboundHandlerAdapter {
-    private static Logger logger = LoggerFactory
-        .getLogger(ServerDemoInHandler.class);
 
+    private static Logger logger = LoggerFactory.getLogger(ServerDemoInHandler.class);
 
-
+    /**
+     * 根据channel
+     * 
+     * @param ctx
+     * @return
+     */
+    public static String getIPString(ChannelHandlerContext ctx) {
+        String ipString = "";
+        String socketString = ctx.channel().remoteAddress().toString();
+        int colonAt = socketString.indexOf(":");
+        ipString = socketString.substring(1, colonAt);
+        return ipString;
+    }
 
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg)
-        throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 
-        //保存channel
+        // 保存channel
         Server.getMap().put(getIPString(ctx), ctx.channel());
 
         logger.info("ServerDemoInHandler.channelRead");
@@ -40,25 +45,22 @@ public class ServerDemoInHandler extends ChannelInboundHandlerAdapter {
         // 接收并打印客户端的信息
         System.out.println("Client said:" + resultStr);
 
-        while (true){
+        while (true) {
             // 向客户端发送消息
-            String message = (String)getMessage();
-            if (message!= null) {
+            String message = (String) getMessage();
+            if (message != null) {
                 Channel channel = Server.getMap().get("127.0.0.1");
                 ByteBuf byteBuf = Unpooled.wrappedBuffer(message.getBytes());
-                channel.writeAndFlush(byteBuf).addListener(new ChannelFutureListener(){
+                channel.writeAndFlush(byteBuf).addListener(new ChannelFutureListener() {
+
                     @Override
-                    public void operationComplete(ChannelFuture future)
-                        throws Exception {
+                    public void operationComplete(ChannelFuture future) throws Exception {
                         logger.info("Server发送消息成功！");
                     }
                 });
 
             }
         }
-
-
-
 
     }
 
@@ -67,26 +69,10 @@ public class ServerDemoInHandler extends ChannelInboundHandlerAdapter {
         ctx.flush();
     }
 
-    /**
-     * 根据channel
-     * @param ctx
-     * @return
-     */
-    public static String getIPString(ChannelHandlerContext ctx){
-        String ipString = "";
-        String socketString = ctx.channel().remoteAddress().toString();
-        int colonAt = socketString.indexOf(":");
-        ipString = socketString.substring(1, colonAt);
-        return ipString;
-    }
-
-
-
-
     private Object getMessage() throws InterruptedException {
-        //模拟下数据生成，每隔5秒产生一条消息
+        // 模拟下数据生成，每隔5秒产生一条消息
         Thread.sleep(5000);
-        //return "message";
+        // return "message";
         return null;
     }
 }
