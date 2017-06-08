996ac527432abb1c74cda988f129968d84a4ec62
CHE Yulin <yche@cse.ust.hk>
2017-06-08 11:11:01 +0800

    refactor and finish chunk computation

--- src/main/java/com/alibaba/middleware/race/sync/server/ChunkComputation.java
+++ src/main/java/com/alibaba/middleware/race/sync/server/ChunkComputation.java
@@ -1,5 +1,7 @@
 package com.alibaba.middleware.race.sync.server;
 
+import com.alibaba.middleware.race.sync.play.Record;
+
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -23,71 +25,90 @@ public class ChunkComputation {
     private RecordLazyEval recordLazyEval;
     private StringBuilder stringBuilder = new StringBuilder();
 
+    private void updateOtherFieldContents(RecordUpdate recordUpdate) {
+        // update contents if possible
+        while (recordLazyEval.hasNext()) {
+            AbstractMap.SimpleEntry<String, Object> entry = recordLazyEval.next();
+            recordUpdate.addEntryIfNotThere(entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void actForDeleteOperation() {
+        // ?-delete mode, it is last operation for this record
+        RecordUpdate recordUpdate = new RecordUpdate(recordLazyEval);
+        deadKeys.put(recordUpdate.firstKey, recordUpdate);
+    }
+
+    private void actForInsertOperation() {
+        if (deadKeys.containsKey(recordLazyEval.curPKVal)) {
+            // insert-delete mode, prune it
+            deadKeys.remove(recordLazyEval.curPKVal);
+        } else {
+            RecordUpdate recordUpdate = activeKeys.get(recordLazyEval.curPKVal);
+            if (recordUpdate == null) {
+                // insert-insert mode
+                recordUpdate = new RecordUpdate(recordLazyEval);
+                insertOnlyUpdates.add(recordUpdate);
+            }
+            // else: insert-update mode, do nothing with active keys
+            recordUpdate.isFirstInsertInChunk = true;
+            updateOtherFieldContents(recordUpdate);
+        }
+    }
+
+    private void actForUpdateOperation() {
+        RecordUpdate recordUpdate = deadKeys.get(recordLazyEval.curPKVal);
+        if (recordUpdate != null) {
+            // update-delete mode
+            if (recordLazyEval.isPKUpdate()) {
+                // in dead keys: remove previous key, add new key
+                deadKeys.remove(recordUpdate.firstKey);
+                recordUpdate.setFirstKey(recordLazyEval.prevPKVal);
+                deadKeys.put(recordUpdate.firstKey, recordUpdate);
+            }
+        } else {
+            recordUpdate = activeKeys.get(recordLazyEval.curPKVal);
+            if (recordUpdate != null) {
+                // update-update mode
+                if (recordLazyEval.isPKUpdate()) {
+                    // in active keys: remove previous key, add new key
+                    activeKeys.remove(recordUpdate.firstKey);
+                    recordUpdate.setFirstKey(recordLazyEval.prevPKVal);
+                    activeKeys.put(recordUpdate.firstKey, recordUpdate);
+                }
+            } else {
+                // ?-update mode, it is the last operation for this record
+                recordUpdate = new RecordUpdate(recordLazyEval);
+                activeKeys.put(recordUpdate.firstKey, recordUpdate);
+            }
+
+            // update contents if possible
+            updateOtherFieldContents(recordUpdate);
+        }
+    }
+
     // used for each chunk computation
-    public void compute(ArrayList<String> fileChunk, int upper_idx, int lower_idx) {
+    public ArrayList<RecordUpdate> compute(ArrayList<String> fileChunk, int upper_idx, int lower_idx) {
         activeKeys.clear();
         deadKeys.clear();
+        insertOnlyUpdates.clear();
 
         for (int i = upper_idx; i > lower_idx; i--) {
             recordLazyEval = new RecordLazyEval(fileChunk.get(i), stringBuilder);
 
-            // 1st: delete-operation, 2nd: insert-operation, 3rd: update-operation
             if (recordLazyEval.operationType == DELETE_OPERATION) {
-                // ?-delete mode, it is last operation for this record
-                deadKeys.put(recordLazyEval.prevPKVal, new RecordUpdate(recordLazyEval.prevPKVal));
+                actForDeleteOperation();
             } else if (recordLazyEval.operationType == INSERT_OPERATION) {
-                if (deadKeys.containsKey(recordLazyEval.curPKVal)) {
-                    // insert-delete mode, prune it
-                    deadKeys.remove(recordLazyEval.curPKVal);
-                } else {
-                    RecordUpdate recordUpdate = activeKeys.get(recordLazyEval.curPKVal);
-                    if (recordUpdate != null) {
-                        // insert-update mode
-                        recordUpdate.setFirstKey(recordLazyEval.curPKVal);
-                    } else {
-                        // insert-insert mode
-                        recordUpdate = new RecordUpdate(recordLazyEval.curPKVal);
-                        insertOnlyUpdates.add(recordUpdate);
-                    }
-                    // update contents if possible
-                    while (recordLazyEval.hasNext()) {
-                        AbstractMap.SimpleEntry<String, Object> entry = recordLazyEval.next();
-                        recordUpdate.addEntryIfNotThere(entry.getKey(), entry.getValue());
-                    }
-                }
+                actForInsertOperation();
             } else if (recordLazyEval.operationType == UPDATE_OPERATION) {
-                RecordUpdate recordUpdate = deadKeys.get(recordLazyEval.curPKVal);
-                if (recordUpdate != null) {
-                    // update-delete mode
-                    if (recordLazyEval.isPKUpdate()) {
-                        // in dead keys: remove previous key, add new key
-                        deadKeys.remove(recordUpdate.firstKey);
-                        recordUpdate.setFirstKey(recordLazyEval.prevPKVal);
-                        deadKeys.put(recordUpdate.firstKey, recordUpdate);
-                    }
-                } else {
-                    recordUpdate = activeKeys.get(recordLazyEval.curPKVal);
-                    if (recordUpdate != null) {
-                        // update-update mode
-                        if (recordLazyEval.isPKUpdate()) {
-                            // in active keys: remove previous key, add new key
-                            activeKeys.remove(recordUpdate.firstKey);
-                            recordUpdate.setFirstKey(recordLazyEval.prevPKVal);
-                            activeKeys.put(recordUpdate.firstKey, recordUpdate);
-                        }
-                    } else {
-                        // ?-update mode, it is the last operation for this record
-                        recordUpdate = new RecordUpdate(recordLazyEval.prevPKVal, recordLazyEval.curPKVal);
-                        activeKeys.put(recordLazyEval.prevPKVal, recordUpdate);
-                    }
-
-                    // update contents if possible
-                    while (recordLazyEval.hasNext()) {
-                        AbstractMap.SimpleEntry<String, Object> entry = recordLazyEval.next();
-                        recordUpdate.addEntryIfNotThere(entry.getKey(), entry.getValue());
-                    }
-                }
+                actForUpdateOperation();
             }
         }
+
+        ArrayList<RecordUpdate> myArrayList = new ArrayList<>(deadKeys.size() + activeKeys.size() + insertOnlyUpdates.size());
+        myArrayList.addAll(insertOnlyUpdates);
+        myArrayList.addAll(deadKeys.values());
+        myArrayList.addAll(activeKeys.values());
+        return myArrayList;
     }
 }
--- src/main/java/com/alibaba/middleware/race/sync/server/RecordUpdate.java
+++ src/main/java/com/alibaba/middleware/race/sync/server/RecordUpdate.java
@@ -1,10 +1,11 @@
 package com.alibaba.middleware.race.sync.server;
 
 
-import com.alibaba.middleware.race.sync.play.Record;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.alibaba.middleware.race.sync.Constants.DELETE_OPERATION;
+import static com.alibaba.middleware.race.sync.Constants.INSERT_OPERATION;
 
 /**
  * Created by yche on 6/6/17.
@@ -12,20 +13,30 @@ import java.util.Map;
 public class RecordUpdate {
     long firstKey;
     final long lastKey;
+    boolean isFirstInsertInChunk;
+    final boolean isLastDeleteInChunk;
 
     // lazy construction
     private Map<String, Object> filedUpdateMap = null;
 
     // used for update
-    public RecordUpdate(long firstKey, long lastKey) {
-        this.firstKey = firstKey;
-        this.lastKey = lastKey;
-    }
-
-    // used for insert and delete
-    public RecordUpdate(long key) {
-        this.firstKey = key;
-        this.lastKey = key;
+    public RecordUpdate(RecordLazyEval recordLazyEval) {
+        if (recordLazyEval.operationType == DELETE_OPERATION) {
+            isLastDeleteInChunk = true;
+            isFirstInsertInChunk = false;
+            firstKey = recordLazyEval.prevPKVal;
+            lastKey = recordLazyEval.prevPKVal;
+        } else if (recordLazyEval.operationType == INSERT_OPERATION) {
+            isLastDeleteInChunk = false;
+            isFirstInsertInChunk = true;
+            firstKey = recordLazyEval.curPKVal;
+            lastKey = recordLazyEval.curPKVal;
+        } else {
+            isLastDeleteInChunk = false;
+            isFirstInsertInChunk = false;
+            firstKey = recordLazyEval.prevPKVal;
+            lastKey = recordLazyEval.curPKVal;
+        }
     }
 
     public void setFirstKey(long firstKey) {
