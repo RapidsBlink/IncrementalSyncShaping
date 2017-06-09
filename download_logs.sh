115a97a422b34b39d383a72f83532ffde0821708
William <wang.lp@outlook.com>
2017-06-09 19:11:28 +0800

    change log download url

--- download_logs.sh
+++ download_logs.sh
@@ -2,13 +2,9 @@
 
 teamCode="78921g1clv"
 baseUrl="http://middle2017.oss-cn-shanghai.aliyuncs.com/"
-gcServerLog="gc_server.log"
-gcClientLog="gc_client.log"
 
-warnServerLog="server-${teamCode}-WARN.log.part"
-warnClientLog="client-${teamCode}-WARN.log.part"
-infoServerLog="server-${teamCode}-INFO.log.part"
-infoClientLog="client-${teamCode}-INFO.log.part"
+serverLog="server.log.tar.gz"
+clientLog="client.log.tar.gz"
 
 mkdir -p logs
 
@@ -20,10 +16,9 @@ echo "download log into folder $folderName..."
 
 cd logs/$folderName
 
-wget "$baseUrl$teamCode/$gcServerLog"
-wget "$baseUrl$teamCode/$gcClientLog"
+wget -c "$baseUrl$teamCode/$serverLog"
+wget -c "$baseUrl$teamCode/$clientLog"
 
-wget "$baseUrl$teamCode/$warnServerLog"
-wget "$baseUrl$teamCode/$warnClientLog"
-wget "$baseUrl$teamCode/$infoClientLog"
-wget "$baseUrl$teamCode/$infoServerLog"
+tar xvzf ${serverLog}
+tar xvzf ${clientLog}
+
