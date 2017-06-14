edcb8114ec69fe9b87b1e4d2262c79bea71a7a79
CHE Yulin <yche@cse.ust.hk>
2017-06-14 22:13:06 +0800

    add new log

--- run_server.sh
+++ run_server.sh
@@ -2,9 +2,9 @@
 
 JAVA_OPS="-XX:InitialHeapSize=3221225472 -XX:MaxDirectMemorySize=209715200 -XX:MaxHeapSize=3221225472 -XX:MaxNewSize=1073741824 -XX:MaxTenuringThreshold=6 -XX:NewSize=1073741824 -XX:OldPLABSize=16 -XX:OldSize=2147483648 -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+UseCompressedOops -XX:+UseConcMarkSweepGC -XX:+UseParNewGC"
 jarPath="./target/sync-1.0.jar"
-schema="middleware3"
+schema="middleware"
 tableName="student"
-start=600
+start=0
 end=700000000
 
 
