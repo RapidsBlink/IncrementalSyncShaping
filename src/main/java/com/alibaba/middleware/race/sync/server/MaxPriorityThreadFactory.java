bec744f85ba7a0299f5636b19ed1e73bcc45548c
William <wang.lp@outlook.com>
2017-06-15 13:32:21 +0800

    rewind and add log

---
+++ src/main/java/com/alibaba/middleware/race/sync/server/MaxPriorityThreadFactory.java
@@ -0,0 +1,16 @@
+package com.alibaba.middleware.race.sync.server;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Created by will on 15/6/2017.
+ */
+
+
+public class MaxPriorityThreadFactory implements ThreadFactory{
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.setPriority(Thread.MAX_PRIORITY );
+        return t;
+    }
+}
