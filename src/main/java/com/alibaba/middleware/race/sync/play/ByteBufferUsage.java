b459ed0f75ddea678fef0d16783e82d25ab443c1
CHE Yulin <yche@cse.ust.hk>
2017-06-18 09:05:06 +0800

    update byte buffer usage

--- src/main/java/com/alibaba/middleware/race/sync/play/ByteBufferUsage.java
+++ src/main/java/com/alibaba/middleware/race/sync/play/ByteBufferUsage.java
@@ -15,20 +15,36 @@ public class ByteBufferUsage {
         byte[] dstBytes = new byte[srcBytes.length];
         System.arraycopy(srcBytes, 0, dstBytes, 0, srcBytes.length);
 
+        // position 0, limit srcBytes.length
         ByteBuffer byteBuffer0 = ByteBuffer.wrap(srcBytes);
         ByteBuffer byteBuffer1 = ByteBuffer.wrap(dstBytes);
         System.out.println(byteBuffer0.position() + ", " + byteBuffer0.limit());
         System.out.println(byteBuffer1.position() + ", " + byteBuffer1.limit());
 
         System.out.println(byteBuffer0.equals(byteBuffer1));
-
+        // not changed, after the comparision
         System.out.println(byteBuffer0.position() + ", " + byteBuffer0.limit());
         System.out.println(byteBuffer1.position() + ", " + byteBuffer1.limit());
+
+        ByteBuffer byteBuffer2 = ByteBuffer.allocate(1024 * 1024);
+        byteBuffer2.put(srcBytes);
+        byteBuffer2.flip();
+        System.out.println(byteBuffer0.equals(byteBuffer2));
+
+        byte[] anotherBytes = new byte[20];
+        for (int i = 0; i < 10; i++) {
+            anotherBytes[i] = (byte) (2 * i);
+            anotherBytes[i + 10] = (byte) i;
+        }
+
+        // directly set position 10, limit 10+10
+        ByteBuffer byteBuffer3 = ByteBuffer.wrap(anotherBytes, 10, 10);
+        System.out.println(byteBuffer0.equals(byteBuffer3));
     }
 
     public static void main(String[] args) {
-//        compareByteBuffer();
-        ByteBuffer byteBuffer=ByteBuffer.allocate(1024);
+        compareByteBuffer();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
         System.out.println(byteBuffer.position() + ", " + byteBuffer.limit());
     }
 
