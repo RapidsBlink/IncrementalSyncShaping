d164c138261e569b7b5ff50addc01496e8b07fe4
CHE Yulin <yche@cse.ust.hk>
2017-06-23 22:29:50 +0800

    update constants

--- src/main/java/com/alibaba/middleware/race/sync/Constants.java
+++ src/main/java/com/alibaba/middleware/race/sync/Constants.java
@@ -23,26 +23,26 @@ public interface Constants {
     // ------------ 本地测试可以使用自己的路径--------------//
     //%%%%%%%%%%%%%%%%% change bellow %%%%%%%%%%%%%%%%%
     // 工作主目录
-    String TESTER_HOME = "/tmp/test";
-    // 赛题数据
-    String DATA_HOME = "/tmp/test/canal_data";
-    // 结果文件目录
-    String RESULT_HOME = "/tmp/test/user_result";
-    // 中间结果目录
-    String MIDDLE_HOME = "/home/yche/tmp/middle";
+//    String TESTER_HOME = "/tmp/test";
+//    // 赛题数据
+//    String DATA_HOME = "/tmp/test/canal_data";
+//    // 结果文件目录
+//    String RESULT_HOME = "/tmp/test/user_result";
+//    // 中间结果目录
+//    String MIDDLE_HOME = "/home/yche/tmp/middle";
     //%%%%%%%%%%%%%%%%% change above %%%%%%%%%%%%%%%%%
 
 
     // ------------ 正式比赛指定的路径--------------//
     //%%%%%%%%%%%%%%%%% change bellow %%%%%%%%%%%%%%%%%
     // 工作主目录
-//    String TESTER_HOME = "/home/admin";
-//    // 赛题数据
-//    String DATA_HOME = "/home/admin/canal_data";
-//    // 结果文件目录(client端会用到)
-//    String RESULT_HOME = "/home/admin/sync_results/" + TEAMCODE;
-//    // 中间结果目录（client和server都会用到）
-//    String MIDDLE_HOME = "/home/admin/middle/" + TEAMCODE;
+    String TESTER_HOME = "/home/admin";
+    // 赛题数据
+    String DATA_HOME = "/home/admin/canal_data";
+    // 结果文件目录(client端会用到)
+    String RESULT_HOME = "/home/admin/sync_results/" + TEAMCODE;
+    // 中间结果目录（client和server都会用到）
+    String MIDDLE_HOME = "/home/admin/middle/" + TEAMCODE;
     //%%%%%%%%%%%%%%%%% change above %%%%%%%%%%%%%%%%%
 
     char SPLIT_CHAR = '|';
