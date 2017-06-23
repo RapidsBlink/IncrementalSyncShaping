c201b2a7604508dbd7114fb8ae4046c40972dad7
CHE Yulin <yche@cse.ust.hk>
2017-06-23 22:48:45 +0800

    refactor

--- src/main/java/com/alibaba/middleware/race/sync/server2/PipelinedComputation.java
+++ src/main/java/com/alibaba/middleware/race/sync/server2/PipelinedComputation.java
@@ -17,11 +17,11 @@ public class PipelinedComputation {
     static ExecutorService fileTransformPool = Executors.newFixedThreadPool(TRANSFORM_WORKER_NUM);
     public static RestoreComputation restoreComputation = new RestoreComputation();
 
-    public static BlockingQueue<LogOperation[]> blockingQueue = new ArrayBlockingQueue<>(64);
-    public static BlockingQueue<FileTransformMediatorTask> mediatorTasks = new ArrayBlockingQueue<>(3);
+    static BlockingQueue<LogOperation[]> blockingQueue = new ArrayBlockingQueue<>(64);
+    static BlockingQueue<FileTransformMediatorTask> mediatorTasks = new ArrayBlockingQueue<>(3);
 
-    static ExecutorService computationPool = Executors.newFixedThreadPool(1);
-    static ExecutorService mediatorPool = Executors.newFixedThreadPool(1);
+    private static ExecutorService computationPool = Executors.newFixedThreadPool(1);
+    private static ExecutorService mediatorPool = Executors.newFixedThreadPool(1);
 
     private static ExecutorService evalSendPool = Executors.newFixedThreadPool(16);
 
