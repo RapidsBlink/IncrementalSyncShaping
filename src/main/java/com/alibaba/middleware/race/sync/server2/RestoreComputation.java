2638d705eda592a626d265bc6d5b4baafe01a0b9
CHE Yulin <yche@cse.ust.hk>
2017-06-23 09:13:50 +0800

    use trove hashmap

--- src/main/java/com/alibaba/middleware/race/sync/server2/RestoreComputation.java
+++ src/main/java/com/alibaba/middleware/race/sync/server2/RestoreComputation.java
@@ -1,6 +1,7 @@
 package com.alibaba.middleware.race.sync.server2;
 
-import java.util.HashMap;
+import gnu.trove.map.hash.THashMap;
+
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 
@@ -8,14 +9,14 @@ import java.util.concurrent.ExecutorService;
  * Created by yche on 6/18/17.
  */
 public class RestoreComputation {
-    public HashMap<LogOperation, LogOperation> recordMap = new HashMap<>();
+    public THashMap<LogOperation,LogOperation> recordMap = new THashMap<>(9*1024*1024);
     public HashSet<LogOperation> inRangeRecordSet = new HashSet<>();
 
     void compute(LogOperation[] logOperations) {
         for (int i = 0; i < logOperations.length; i++) {
             LogOperation logOperation = logOperations[i];
             if (logOperation instanceof DeleteOperation) {
-                //recordMap.remove(logOperation);
+                recordMap.remove(logOperation);
                 if (PipelinedComputation.isKeyInRange(logOperation.relevantKey)) {
                     inRangeRecordSet.remove(logOperation);
                 }
@@ -30,7 +31,7 @@ public class RestoreComputation {
                 insertOperation.mergeAnother((UpdateOperation) logOperation); //3
 
                 if (logOperation instanceof UpdateKeyOperation) {
-//                    recordMap.remove(logOperation);
+                    recordMap.remove(logOperation);
                     if (PipelinedComputation.isKeyInRange(logOperation.relevantKey)) {
                         inRangeRecordSet.remove(logOperation);
                     }
