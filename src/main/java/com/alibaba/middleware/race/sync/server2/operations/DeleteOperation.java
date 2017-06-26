8d37d8df4f4a8aaf1a203cb9c7a41c411676d65f
CHE Yulin <yche@cse.ust.hk>
2017-06-26 19:12:43 +0800

    refactor

--- src/main/java/com/alibaba/middleware/race/sync/server2/operations/DeleteOperation.java
+++ src/main/java/com/alibaba/middleware/race/sync/server2/operations/DeleteOperation.java
@@ -1,10 +1,6 @@
 package com.alibaba.middleware.race.sync.server2.operations;
 
 
-import com.alibaba.middleware.race.sync.server2.PipelinedComputation;
-
-import static com.alibaba.middleware.race.sync.server2.RestoreComputation.inRangeRecordSet;
-
 /**
  * Created by yche on 6/19/17.
  */
--- src/main/java/com/alibaba/middleware/race/sync/server2/operations/UpdateOperation.java
+++ src/main/java/com/alibaba/middleware/race/sync/server2/operations/UpdateOperation.java
@@ -1,8 +1,5 @@
 package com.alibaba.middleware.race.sync.server2.operations;
 
-
-import static com.alibaba.middleware.race.sync.server2.RestoreComputation.recordMap;
-
 /**
  * Created by yche on 6/19/17.
  */
