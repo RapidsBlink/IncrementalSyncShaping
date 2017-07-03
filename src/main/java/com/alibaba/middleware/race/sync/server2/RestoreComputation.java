f1f53390fdf9e4c1dbc9ef7c5bfd2ffc9c890ff0
CHE Yulin <yche@cse.ust.hk>
2017-07-03 13:58:47 +0800

    trove hash set not help a lot here

--- src/main/java/com/alibaba/middleware/race/sync/server2/RestoreComputation.java
+++ src/main/java/com/alibaba/middleware/race/sync/server2/RestoreComputation.java
@@ -1,6 +1,7 @@
 package com.alibaba.middleware.race.sync.server2;
 
 import com.alibaba.middleware.race.sync.server2.operations.*;
+import gnu.trove.set.hash.THashSet;
 
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
@@ -11,7 +12,7 @@ import java.util.concurrent.ExecutorService;
 public class RestoreComputation {
     public static YcheHashMap recordMap = new YcheHashMap(20 * 1024 * 1024);
 
-    public static HashSet<LogOperation> inRangeRecordSet = new HashSet<>();
+    public static THashSet<LogOperation> inRangeRecordSet = new THashSet<>(4 * 1024 * 1024);
 
     static void compute(LogOperation[] logOperations) {
         for (int i = 0; i < logOperations.length; i++) {
